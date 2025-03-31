1bd6eae75035c57f420a3489a17b832fbb205e9c
kirubel4 <kirubelayele2127@gmail.com>
2025-03-31 20:17:10 +0300

    properties page

--- frontend/src/pages/Properties.jsx
+++ frontend/src/pages/Properties.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function Properties(){
     return(
-        <h1>aklsdfjlaskfjaksfjlaksjflaks</h1>
+        <h1>hello Properties</h1>
     )
 }
 export default Properties;
