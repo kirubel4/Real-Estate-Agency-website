5ec286c3d44ec3c35fb21bd99ed861b5421f6665
kirubel4 <kirubelayele2127@gmail.com>
2025-04-05 20:17:00 +0300

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 function About(){
     return(
-        <h1 className="dark:text-white">what a nice thing to know about your self</h1>
+        <div>
+            <h1 className="" >Ayele real estate</h1>
+            <h1 className="dark:text-white">what a nice thing to know about your self</h1>
+        </div>
+        
     )
 }
 export default About;
