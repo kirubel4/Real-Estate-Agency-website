01b49cf1a71476130450b2128c7ac553accb3ac0
kirubel4 <kirubelayele2127@gmail.com>
2025-04-12 16:14:17 +0300

    updated

--- frontend/src/pages/Service.jsx
+++ frontend/src/pages/Service.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Nav from "../componets/Nav";
 import Footer from "../componets/Footer";
 import DarkModeToggle from "../componets/DarkMode";
+import Services from "../componets/Services";
 function Service(){
     return(
         <>
         <Nav />
+        <Services />
         <Footer/>
         <DarkModeToggle />
         </>
