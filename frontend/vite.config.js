8b96bb962774bcbb84a7059f8fa987edcedc196a
kirubel4 <kirubelayele2127@gmail.com>
2025-04-24 16:41:20 +0300

    created

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/detail': 'http://localhost:5000', // Proxy any /detail/* to Express
+    },
+  },
 })
