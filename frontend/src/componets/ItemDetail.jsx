4d8b4cc664c4027bb4829d3ef01ffaccbc22ceef
kirubel4 <kirubelayele2127@gmail.com>
2025-05-10 15:09:41 +0300

    added

--- frontend/src/componets/ItemDetail.jsx
+++ frontend/src/componets/ItemDetail.jsx
@@ -13,14 +13,10 @@ function ItemDetail(){
         const fetchItem = async()=>{
             try{
                 const resp = await fetch(`/detail/${id}`);
-                if(resp.ok){
-                    const data = await resp.json();
-                    setItems(data);
+                
+                const data = await resp.json();
+                setItems(data);
                     
-                }else{
-                    console.log("error in item(is not found)")
-                }
-
             }catch(error){
                 console.error('Error fetching item details:', error);
             }
@@ -30,6 +26,7 @@ function ItemDetail(){
     if (!item) return <p>Loading...</p>;
     return(
         <>
+            {/* <div className="rounded cursor-pointer" onClick={()=>{navigate(-1)}}> */}
             <div className="rounded cursor-pointer" onClick={()=>{navigate(-1)}}>
                 <IoIosArrowBack size={35} className="bg-slate-300 rounded-full hover:scale-90"/>
             </div>
@@ -48,14 +45,14 @@ function ItemDetail(){
       {/* Photos List */}
       <div className="mt-6">
         <h2 className="text-xl font-medium text-gray-800">Photos</h2>
-        <div className="flex flex-wrap mt-4 gap-4">
+        <div className="flex flex-wrap mt-8 gap-x-10 gap-y-8">
         {item.photos && item.photos.map((photo, index) => (
-  <img
-    key={index}
-    src={`data:image/jpeg;base64,${photo}`}
-    alt={`Photo ${index + 1}`}
-    className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-48 object-cover rounded-md"
-  />
+        <img
+          key={index}
+          src={`data:image/jpeg;base64,${photo}`}
+          alt={`Photo ${index + 1}`}
+          className="w-full sm:w-1 md:w-1/3 lg:w-1/3 xl:w-1/4 h-auto object-cover rounded-md"
+        />
 ))}
         </div>
       </div>
