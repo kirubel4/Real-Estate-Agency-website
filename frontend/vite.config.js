679de662d28330d3b2f9fe3e4eacd0a42f07eb3a
kirubel4 <kirubelayele2127@gmail.com>
2025-05-10 15:56:04 +0300

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     proxy: {
-      '/detail': 'http://localhost:5000', // Proxy any /detail/* to Express
+      '/detail': 'http://localhost:5000',
     },
   },
 })
